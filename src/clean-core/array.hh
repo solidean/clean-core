7a78f50450c8a1a424d096e0de41841c8b31bd61
Philip Trettner <trettner@shapedcode.com>
2025-12-22 10:31:18 +0100

    small glow up

--- src/clean-core/array.hh
+++ src/clean-core/array.hh
@@ -21,37 +21,37 @@
 template <class T>
 struct cc::array : private cc::allocating_container<T, array<T>>
 {
-    using container_impl = cc::allocating_container<T, array<T>>;
+    using base = cc::allocating_container<T, array<T>>;
 
     // element access
 public:
-    using container_impl::operator[]; // access element by index
-    using container_impl::back;       // access last element
-    using container_impl::data;       // get pointer to underlying storage
-    using container_impl::front;      // access first element
+    using base::operator[]; // access element by index
+    using base::back;       // access last element
+    using base::data;       // get pointer to underlying storage
+    using base::front;      // access first element
 
     // iterators
 public:
-    using container_impl::begin; // get iterator to first element
-    using container_impl::end;   // get iterator to one past last element
+    using base::begin; // get pointer to first element
+    using base::end;   // get pointer to one past last element
 
     // queries
 public:
-    using container_impl::empty;      // check if array is empty
-    using container_impl::size;       // get number of elements
-    using container_impl::size_bytes; // get total size in bytes
+    using base::empty;      // check if array is empty
+    using base::size;       // get number of elements
+    using base::size_bytes; // get total size in bytes
 
     // factories
 public:
-    using container_impl::create_copy_of;         // create deep copy from span
-    using container_impl::create_defaulted;       // create with default-constructed elements
-    using container_impl::create_filled;          // create with copies of a value
-    using container_impl::create_from_allocation; // create from existing allocation
-    using container_impl::create_uninitialized;   // create with uninitialized memory
+    using base::create_copy_of;         // create deep copy from span
+    using base::create_defaulted;       // create with default-constructed elements
+    using base::create_filled;          // create with copies of a value
+    using base::create_from_allocation; // create from existing allocation
+    using base::create_uninitialized;   // create with uninitialized memory
 
     // allocation management
 public:
-    using container_impl::extract_allocation; // extract underlying allocation
+    using base::extract_allocation; // extract underlying allocation
 
     // array has deep-copy value semantics
     array() = default;
@@ -61,7 +61,7 @@ public:
     array(array const&) = default;
     array& operator=(array const&) = default;
 
-    friend container_impl;
+    friend base;
 };
 
 /// Fixed-size array of exactly N elements of type T.
